9ed448ae82c71c78b9907e9fc9171babcdd32aa8
mati199oo <mati199oo@gmail.com>
2016-12-04 22:44:58 +0100

    Aded clusters and documentation

--- src/song_comparator.py
+++ src/song_comparator.py
@@ -3,6 +3,54 @@ from difflib import SequenceMatcher
 
 db_file = "../resources/songs.db"
 
+def print_all_clusters(clusters):
+    print "Clusters: "
+    print ""
+    for cluster in clusters:
+        for song in cluster:
+            print str(song[0]) + ", " + str(song[1])
+        print ""
+        print ""
+
+def create_cluster(cluster, songs, song, similar_artists):
+    song_list = []
+    if songs:
+        for index in xrange(0, len(songs)):
+            similarity = calculate_similarity(song, songs[index], similar_artists)
+            if similarity > 5.0:
+                song_list.append(songs[index])
+            else:
+                cluster.append([songs[index][0], songs[index][14]])
+    return song_list, cluster
+
+def create_list_from_records(songs_data):
+    songs = []
+    for song in songs_data:
+        songs.append(song)
+    return songs
+
+def get_all_songs_data(con):
+    query = "SELECT s.title, s.year, s.release, s.song_hotttnesss, s.danceability, s.duration, " \
+            "s.loudness, s.end_of_fade_in, s.start_of_fade_out, s.key, s.energy, s.mode, s.tempo, " \
+            "s.time_signature, a.artist_name, a.artist_familiarity, a.artist_hotttnesss, a.artist_latitude, " \
+            "a.artist_location, a.artist_longitude, a.artist_id FROM Songs AS s inner join Artists AS a ON " \
+            "s.artist_id = a.a_id"
+    return con.execute(query)
+
+def cluster_songs(con):
+    songs_data = get_all_songs_data(con)
+    songs_data = create_list_from_records(songs_data)
+    clusters = []
+    while songs_data:
+        cluster = []
+        song = songs_data[0]
+        cluster.append([song[0], song[14]])
+        del songs_data[0]
+        similar_artists = get_similar_artists(con, song[20])
+        songs_data, cluster = create_cluster(cluster, songs_data, song, similar_artists)
+        clusters.append(cluster)
+    print_all_clusters(clusters)
+
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
@@ -114,10 +162,12 @@ def print_database_statistics(con):
 def main():
     con = sql.connect(db_file)
     con.text_factory = str
-    #print_database_statistics(con)
+    # print_database_statistics(con)
 
-    song_name = "The Deceived"
-    number_of_songs = 20
-    print_similar_songs(con, song_name, number_of_songs)
+    # song_name = "The Deceived"
+    # number_of_songs = 20
+    # print_similar_songs(con, song_name, number_of_songs)
+
+    cluster_songs(con)
 
 main()
